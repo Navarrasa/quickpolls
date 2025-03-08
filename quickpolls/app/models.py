4ad573958177f2aef153e86558759e825d072c15
Gustavo <gugubrunopf@gmail.com>
2025-03-07 22:09:46 -0300

    starting the journey

---
+++ quickpolls/app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+# Models are the Data Bank of the website, it will be the creator of all data that will be storaged in the SQLite archive.
+
+class Users(models.Model):
+    pass
+
+class Polls(models.Model):
+    pass
+
+class Votes(models.Model):
+    pass
+
+class Categories(models.Model):
+    pass
